f236aed8c2235b78023ff3b334ed131cb2ec0093
ferdy <xv.ferdy@gmail.com>
2020-05-07 08:44:52 +0700

    Notifying the Outside World

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callback) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
-
+        if (callback) {
+            this.onStart = callback.onStart;
+        }
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
     start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
         this.tick();
         this.interval = setInterval(this.tick, 1000);
     }
@@ -37,4 +42,8 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log('Timer started');
+    }
+});
