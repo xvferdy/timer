60dafb3e8a5a8e2c267d7d07e22340db147ebb56
ferdy <xv.ferdy@gmail.com>
2020-05-20 05:28:11 +0700

    all

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+//selecting elements
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle'); //circle
 
+//getting the circle's value
+const perimeter = circle.getAttribute('r') * 2 * Math.PI; //panjang garis lingkaran
+circle.setAttribute('stroke-dasharray', perimeter); //membuat nilai stroke-dasharray (akan full karena rumus perimeter)
+
+let duration;
+
+//buat class timer
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log('Timer started');
+    onStart(totalDuration) {
+        duration = totalDuration;
     },
-    onTick() {
-        console.log('Timer just ticked down');
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset', perimeter * timeRemaining / duration - perimeter);
     },
     onComplete() {
         console.log('Timer is completed');
--- timer.js
+++ timer.js
@@ -1,45 +1,54 @@
 class Timer {
     constructor(durationInput, startButton, pauseButton, callback) {
+        //inisialisasi
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
-        if (callback) {
+        if (callback) { //jika ada argumen callback maka buat 3 properti sebagai berikut
             this.onStart = callback.onStart;
             this.onTick = callback.onTick;
             this.onComplete = callback.onComplete;
         }
+
+        //event, menjalankan fungsi di prototype
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
+    //ketika start di klik
     start = () => {
         if (this.onStart) {
-            this.onStart();
+            this.onStart(this.timeRemaining);
         }
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
+        this.tick(); //supaya langsung jalan dulu, tidak tunggu 1000ms baru dijalankan
+        this.interval = setInterval(this.tick, 20);
     }
+
+    //ke-trigger saat start dijalankan
     tick = () => {
         if (this.timeRemaining <= 0) {
-            this.pause();
+            this.pause(); //paused when it hits 0.0 sec
             if (this.onComplete) {
                 this.onComplete();
             }
         } else {
-            this.timeRemaining = this.timeRemaining - 1;
+            this.timeRemaining = this.timeRemaining - 0.02;
             if (this.onTick) {
-                this.onTick();
+                this.onTick(this.timeRemaining);
             }
         }
     }
+    //menghentikan setInterval
     pause = () => {
         clearInterval(this.interval);
     }
 
+    //mengambil inputan pada input elemen
     get timeRemaining() {
         return parseFloat(this.durationInput.value);
     }
-    set timeRemaining(time) {
-        this.durationInput.value = time
+    //mengupdate inputan pada input elemen
+    set timeRemaining(time) { //set method membutuhkan parameter
+        this.durationInput.value = time.toFixed(2)
     }
 }
